d599665940c5050c186c36e7226a44571d7acd29
Yaralisa <yaralisa28@gmail.com>
2024-10-02 11:51:50 +0000

    proyecto finalizado

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../../styles/footer.css'; // Asegúrate de tener este archivo CSS para los estilos del pie de página
+import { FooterYara } from "./footerYara";
 
 export const Footer = () => {
     return (
         <footer className="footer text-center py-3 mt-5 mb-5">
-            <Link to="/" className="back-to-top shadow">Back to top</Link>
             <p className="footer-text">© 2024 Star Wars Blog</p>
+            <FooterYara/>
         </footer>
     );
 };
